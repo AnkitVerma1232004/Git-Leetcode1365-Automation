b95c1c89cb0bafc6abcdd609a204e8ff4868b3df
AnkitVerma1232004 <120023192+AnkitVerma1232004@users.noreply.github.com>
2025-04-30 19:44:51 +0530

    Update test.c

--- tests/test.c
+++ tests/test.c
@@ -22,6 +22,9 @@ void testCase(int* nums, int n, int* expected) {
         printf("PASS (%ldms)\n", (end - start));
     } else {
         printf("FAIL (%ldms)\n", (end - start));
+        printf("  Input:    ");
+        for (int i = 0; i < n; i++) printf("%d ", nums[i]);
+        printf("\n");
         printf("  Expected: ");
         for (int i = 0; i < n; i++) printf("%d ", expected[i]);
         printf("\n  Got:      ");
@@ -33,17 +36,17 @@ void testCase(int* nums, int n, int* expected) {
 }
 
 int main() {
-    int t1[]  = {8,1,2,2,3},        e1[]  = {4,0,1,1,3};      testCase(t1,5,e1);
-    int t2[]  = {6,5,4,8},          e2[]  = {2,1,0,3};      testCase(t2,4,e2);
-    int t3[]  = {7,7,7,7},          e3[]  = {0,0,0,0};      testCase(t3,4,e3);
-    int t4[]  = {1},                e4[]  = {0};            testCase(t4,1,e4);
-    int t5[]  = {3,2,1},            e5[]  = {2,1,0};        testCase(t5,3,e5);
-    int t6[]  = {1,2,2,3,4},        e6[]  = {0,1,1,2,3};    testCase(t6,5,e6);
-    int t7[]  = {9,7,5,6,8},        e7[]  = {4,2,0,1,3};    testCase(t7,5,e7);
-    int t8[]  = {5,4,3,2,1},        e8[]  = {4,3,2,1,0};    testCase(t8,5,e8);
-    int t9[]  = {10,10,10,10,10},   e9[]  = {0,0,0,0,0};    testCase(t9,5,e9);
-    int t10[] = {0,2,4,6,8,10},     e10[] = {0,1,2,3,4,5};  testCase(t10,6,e10);
-    int t11[] = {-1,-2,-3,-4},      e11[] = {3,2,1,0};     testCase(t11,4,e11);
+    int t1[]  = {8,1,2,2,3},     e1[]  = {4,0,1,1,3};     testCase(t1,5,e1);
+    int t2[]  = {6,5,4,8},       e2[]  = {2,1,0,3};       testCase(t2,4,e2);
+    int t3[]  = {7,7,7,7},       e3[]  = {0,0,0,0};       testCase(t3,4,e3);
+    int t4[]  = {1},             e4[]  = {0};             testCase(t4,1,e4);
+    int t5[]  = {3,2,1},         e5[]  = {2,1,0};         testCase(t5,3,e5);
+    int t6[]  = {1,2,2,3,4},     e6[]  = {0,1,1,2,3};     testCase(t6,5,e6);
+    int t7[]  = {9,7,5,6,8},     e7[]  = {4,2,0,1,3};     testCase(t7,5,e7);
+    int t8[]  = {5,4,3,2,1},     e8[]  = {4,3,2,1,0};     testCase(t8,5,e8);
+    int t9[]  = {10,10,10,10,10},  e9[]  = {0,0,0,0,0};     testCase(t9,5,e9);
+    int t10[] = {0,2,4,6,8,10},    e10[] = {0,1,2,3,4,5};   testCase(t10,6,e10);
+    int t11[] = {-1,-2,-3,-4},    e11[] = {3,2,1,0};     testCase(t11,4,e11);
 
     return any_failed ? EXIT_FAILURE : EXIT_SUCCESS;
 }
