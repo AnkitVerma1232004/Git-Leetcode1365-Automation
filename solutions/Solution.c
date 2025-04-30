331ca2cd782e7307331f5016d08993183c39feaa
AnkitVerma1232004 <120023192+AnkitVerma1232004@users.noreply.github.com>
2025-04-30 17:26:33 +0530

    Update Solution.c

--- solutions/Solution.c
+++ solutions/Solution.c
@@ -1,5 +1,5 @@
 #include <stdlib.h>
-
+//
 int* smallerNumbersThanCurrent(int* nums, int numsSize, int* returnSize) {
     int* res = (int*)malloc(numsSize * sizeof(int));
     for (int i = 1; i < numsSize; i++) {
