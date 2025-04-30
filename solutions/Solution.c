773b1d9e881783de18bc022cbba192e3cb576bda
AnkitVerma1232004 <120023192+AnkitVerma1232004@users.noreply.github.com>
2025-04-30 10:51:34 +0530

    Update Solution.c

--- solutions/Solution.c
+++ solutions/Solution.c
@@ -4,16 +4,5 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* smallerNumbersThanCurrent(int* nums, int numsSize, int* returnSize) {
-    int* res = (int*)malloc(numsSize * sizeof(int));
-    for (int i = 0; i < numsSize; i++) {
-        int count = 0;
-        for (int j = 1; j < numsSize; j++) {
-            if (nums[j] < nums[i]) {
-                count++;
-            }
-        }
-        res[i] = count;
-    }
-    *returnSize = numsSize;
-    return res;
+    
 }
