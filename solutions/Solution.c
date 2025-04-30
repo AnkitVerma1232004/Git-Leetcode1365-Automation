4f75495d0fb575652dd9f9923eb42f36e1ad907a
AnkitVerma1232004 <120023192+AnkitVerma1232004@users.noreply.github.com>
2025-04-30 17:09:16 +0530

    Update Solution.c

--- solutions/Solution.c
+++ solutions/Solution.c
@@ -1,6 +1,7 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
+//yo
 #include <stdlib.h>//yo
 
 int* smallerNumbersThanCurrent(int* nums, int numsSize, int* returnSize) {
