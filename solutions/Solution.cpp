6b7c9dc4d60772ff213802e35e9a98734fa66dbc
AnkitVerma1232004 <120023192+AnkitVerma1232004@users.noreply.github.com>
2025-04-30 11:26:24 +0530

    Update Solution.cpp

--- solutions/Solution.cpp
+++ solutions/Solution.cpp
@@ -1,11 +1,22 @@
-
 #include <vector>
 using namespace std;
-class Solution {
-    public:
-        vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
-            // write your code here
-           
-        }
-    };
 
+class Solution {
+public:
+    vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> res(n);
+        
+        for (int i = 0; i < n; i++) {
+            int count = 0;
+            for (int j = 0; j < n; j++) {
+                if (nums[j] < nums[i]) {
+                    count++;
+                }
+            }
+            res[i] = count;
+        }
+        
+        return res;
+    }
+};
