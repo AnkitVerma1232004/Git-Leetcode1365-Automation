b23bf654b4a9a4156d4787dffc8ed551ad1d305d
AnkitVerma1232004 <120023192+AnkitVerma1232004@users.noreply.github.com>
2025-04-30 19:47:36 +0530

    Update Solution.cpp

--- solutions/Solution.cpp
+++ solutions/Solution.cpp
@@ -1,26 +1,17 @@
 #include <vector>
-#include <algorithm>
-#include <unordered_map>
 using namespace std;
 
 class Solution {
 public:
     vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
-        vector<int> sorted = nums;
-        sort(sorted.begin(), sorted.end());
-        
-        unordered_map<int, int> countMap;
-        int count = 0;
-        for (int i = 0; i < sorted.size(); i++) {
-            if (i == 0 || sorted[i] != sorted[i-1]) {
-                countMap[sorted[i]] = count;
+        int n = nums.size();
+        vector<int> res(n);
+        for (int i = 0; i < n; i++) {
+            int cnt = 0;
+            for (int j = 0; j < n; j++) {
+                if (nums[j] < nums[i]) cnt++;
             }
-            count++;
-        }
-        
-        vector<int> res(nums.size());
-        for (int i = 0; i < nums.size(); i++) {
-            res[i] = countMap[nums[i]];
+            res[i] = cnt;
         }
         return res;
     }
