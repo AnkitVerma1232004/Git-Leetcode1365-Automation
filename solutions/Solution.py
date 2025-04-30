d92cbb71b788d83ec051810c4a12b66e1b9d2524
AnkitVerma1232004 <120023192+AnkitVerma1232004@users.noreply.github.com>
2025-04-30 19:01:32 +0530

    Update Solution.py

--- solutions/Solution.py
+++ solutions/Solution.py
@@ -1,10 +1,10 @@
 class Solution:
     def smallerNumbersThanCurrent(self, nums):
-        res = []
-        for i in range(len(nums)):
-            count = 0
-            for j in range(len(nums)):
-                if nums[j] < nums[i]:
-                    count += 1
-            res.append(count)
-        return res
+        sorted_nums = sorted(nums)
+        count_map = {}
+        count = 0
+        for i, num in enumerate(sorted_nums):
+            if i == 0 or sorted_nums[i] != sorted_nums[i-1]:
+                count_map[num] = count
+            count += 1
+        return [count_map[num] for num in nums]
