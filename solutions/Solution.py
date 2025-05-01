f29c8a327104299bd3499fb5359ba54e7eefb720
AnkitVerma1232004 <120023192+AnkitVerma1232004@users.noreply.github.com>
2025-05-01 11:32:01 +0530

    Update Solution.py

--- solutions/Solution.py
+++ solutions/Solution.py
@@ -2,7 +2,7 @@ class Solution:
     def smallerNumbersThanCurrent(self, nums):
         res = []
         for i in range(len(nums)):
-            count = 1
+            count = 0
             for j in range(len(nums)):
                 if nums[j] < nums[i]:
                     count += 1
