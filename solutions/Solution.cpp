12b53f54ddf68ee7cba2cc24741a7233ed22d2f5
AnkitVerma1232004 <120023192+AnkitVerma1232004@users.noreply.github.com>
2025-05-01 11:40:58 +0530

    Update Solution.cpp

--- solutions/Solution.cpp
+++ solutions/Solution.cpp
@@ -1,18 +1,9 @@
 #include <vector>
 using namespace std;
-//cpp solution file
+// Return the result vector through the function below
 class Solution {
 public:
     vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> res(n);
-        for (int i = 0; i < n; i++) {
-            int cnt = 0;
-            for (int j = 0; j < n; j++) {
-                if (nums[j] < nums[i]) cnt++;
-            }
-            res[i] = cnt;
-        }
-        return res;
+        
     }
 };
