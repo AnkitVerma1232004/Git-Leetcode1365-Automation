d55e0db7214598bdd4f18fabef6a37093e56bf6c
AnkitVerma1232004 <120023192+AnkitVerma1232004@users.noreply.github.com>
2025-05-01 08:24:12 +0530

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -1,4 +1,10 @@
 import time
+import sys
+import os
+
+# Add the parent directory to the Python path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from solutions.Solution import Solution
 
 def test_case(nums, expected):
